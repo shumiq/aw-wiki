8b5299b0d637add71047d197533e985129cc9919
IQ <santiphap.wat@gmail.com>
2025-08-14 15:53:46 +0700

    cache static request

--- src/routes/armoredwarfare_gamepedia/[...path].ts
+++ src/routes/armoredwarfare_gamepedia/[...path].ts
@@ -1,12 +1,26 @@
+const cache = new Map<
+  string,
+  {
+    body: ArrayBuffer;
+    headers: Headers;
+  }
+>();
+
 export async function GET({ params }: { params: { path: string } }) {
+  if (cache.has(params.path)) {
+    return new Response(cache.get(params.path)!.body, {
+      headers: cache.get(params.path)!.headers,
+    });
+  }
   const res = await fetch(
     `https://static.wikia.nocookie.net/armoredwarfare_gamepedia/${params.path}`,
     { headers: { referer: "https://armoredwarfare.fandom.com/" } },
   );
   if (res.status === 404) return new Response(null, { status: 404 });
-  const response = new Response(res.body);
-  for (const [key, value] of res.headers.entries()) {
-    response.headers.set(key, value);
-  }
-  return response;
+  const buffer = await res.arrayBuffer();
+  cache.set(params.path, {
+    body: buffer,
+    headers: res.headers,
+  });
+  return new Response(buffer, { headers: res.headers });
 }
